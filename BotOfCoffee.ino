435e13b4b99b1c42ee6666d9ba3e3b306b0f4561
chrisbrasington <chrisbrasington@gmail.com>
2015-07-28 18:31:32 -0600

    makefile

--- BotOfCoffee.ino
+++ BotOfCoffee.ino
@@ -1,8 +1,8 @@
-#include <SPI.h>         
+#include <SPI.h>
 #include <Ethernet.h>
 #include <EthernetUdp.h>
 #include <Time.h>
-#include <String>
+#include <String.h>
 #include <Twitter.h>
 
 // Enter a MAC address for your controller below.
@@ -24,87 +24,6 @@ Twitter twitter("");
 
 // 10 second refresh interval
 int delayInterval = 10000;
-
-void setup() 
-{
- // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-   while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
-
-  // start Ethernet and UDP
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
-    // no point in carrying on, so do nothing forevermore:
-    for(;;)
-      ;
-  }
-  Udp.begin(localPort);
-  
-  sendNTPpacket(timeServer); // send an NTP packet to a time server
-
-  // wait to see if a reply is available
-  // set current time
-  delay(1000);  
-  if ( Udp.parsePacket() ) {  
-    // We've received a packet, read the data from it
-    Udp.read(packetBuffer,NTP_PACKET_SIZE);  // read the packet into the buffer
-
-    //the timestamp starts at byte 40 of the received packet and is four bytes,
-    // or two words, long. First, esxtract the two words:
-
-    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);  
-    // combine the four bytes (two words) into a long integer
-    // this is NTP time (seconds since Jan 1 1900):
-    unsigned long secsSince1900 = highWord << 16 | lowWord;            
-
-    // now convert NTP time into everyday time:
-    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-    const unsigned long seventyYears = 2208988800UL;     
-    // subtract seventy years:
-    unsigned long epoch = secsSince1900 - seventyYears;                        
-    setTime(epoch);
-  }
-  
-  // initial tweet of reboot
-  tweet("I'm being programmed! @BotOfCoffee has rebooted.");
-}
-
-void loop()
-{
-  // serial print current time
-  Serial.print("Current Time is ");
-  Serial.println(getTimeString());
-  
-  // wait ten seconds before asking for the time again
-  delay(delayInterval); 
-}
-
-bool tweet(String message){ 
-  Serial.println("Tweeting message: ");
-  Serial.println(message);
-  
-  message += " " + getTimeString();
-  
-  if (twitter.post(message.c_str())) {
-  // Specify &Serial to output received response to Serial.
-  // If no output is required, you can just omit the argument, e.g.
-  // int status = twitter.wait();
-  int status = twitter.wait(&Serial);
-  if (status == 200) {
-    Serial.println("OK.");
-    return true;
-  } else {
-    Serial.print("failed : code ");
-    Serial.println(status);
-  }
-  } else {
-  Serial.println("connection failed.");
-  }
-  return false;
-}
 
 // DD/MM/YYYY HH:MM:SS AM/PM
 String getTimeString() {
@@ -171,9 +90,86 @@ unsigned long sendNTPpacket(IPAddress& address)
   Udp.endPacket(); 
 }
 
+bool tweet(String message){
+  Serial.println("Tweeting message: ");
+  Serial.println(message);
 
+  message += " " + getTimeString();
 
+  if (twitter.post(message.c_str())) {
+    // Specify &Serial to output received response to Serial.
+    // If no output is required, you can just omit the argument, e.g.
+    // int status = twitter.wait();
+    int status = twitter.wait(&Serial);
+    if (status == 200) {
+      Serial.println("OK.");
+      return true;
+    } else {
+      Serial.print("failed : code ");
+      Serial.println(status);
+    }
+  } else {
+    Serial.println("connection failed.");
+  }
+  return false;
+}
 
+void setup()
+{
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+  // start Ethernet and UDP
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // no point in carrying on, so do nothing forevermore:
+    for(;;)
+      ;
+  }
+  Udp.begin(localPort);
+
+  sendNTPpacket(timeServer); // send an NTP packet to a time server
+
+  // wait to see if a reply is available
+  // set current time
+  delay(1000);
+  if ( Udp.parsePacket() ) {
+    // We've received a packet, read the data from it
+    Udp.read(packetBuffer,NTP_PACKET_SIZE);  // read the packet into the buffer
+
+    //the timestamp starts at byte 40 of the received packet and is four bytes,
+    // or two words, long. First, esxtract the two words:
+
+    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+    // combine the four bytes (two words) into a long integer
+    // this is NTP time (seconds since Jan 1 1900):
+    unsigned long secsSince1900 = highWord << 16 | lowWord;
+
+    // now convert NTP time into everyday time:
+    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+    const unsigned long seventyYears = 2208988800UL;
+    // subtract seventy years:
+    unsigned long epoch = secsSince1900 - seventyYears;
+    setTime(epoch);
+  }
+
+  // initial tweet of reboot
+  tweet("I'm being programmed! @BotOfCoffee has rebooted.");
+}
+
+void loop()
+{
+  // serial print current time
+  Serial.print("Current Time is ");
+  Serial.println(getTimeString());
+
+  // wait ten seconds before asking for the time again
+  delay(delayInterval);
+}
 
 
 
---
+++ Makefile
@@ -0,0 +1,6 @@
+#BOARD_TAG = pro5v328
+BOARD_TAG = uno
+
+ARDUINO_PORT = /dev/ttyACM0
+
+include /usr/share/arduino/Arduino.mk
